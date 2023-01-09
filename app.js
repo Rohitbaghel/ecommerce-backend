6fa35e520974c4587502e9e738b24e2530dd6c42
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2023-01-09 11:54:09 +0530

    header added

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(
   cors({
     origin: "*",
