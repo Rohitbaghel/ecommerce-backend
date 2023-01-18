b1fb45c22c67232e5af7998639a26a440f3c42de
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2023-01-18 23:15:20 +0530

    cookie update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,7 +10,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
